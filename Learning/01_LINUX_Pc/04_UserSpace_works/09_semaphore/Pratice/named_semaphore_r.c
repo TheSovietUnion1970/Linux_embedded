ba8475e2b11ae69c22b9a3f98c8b6298f25c49c3
Vinh <Vdang3900@gmail.com>
2025-04-12 15:54:48 +0700

    [Pc] - update and add UserSpace_works

---
+++ Learning/01_LINUX_Pc/04_UserSpace_works/09_semaphore/Pratice/named_semaphore_r.c
@@ -0,0 +1,25 @@
+// named_semaphore_reader.c
+#include <stdio.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+int sem_v = 0;
+
+int main() {
+    sem_t *sem = sem_open("/my_named_semaphore", O_CREAT, 0644, 0);
+
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        return 1;
+    }
+
+    printf("Reader: Waiting on semaphore...\n");
+    sem_wait(sem);
+    sem_getvalue(sem, &sem_v);
+    printf("Sem value = %d\n", sem_v);
+    printf("Reader: Received signal!\n");
+
+    sem_close(sem);
+    sem_unlink("/my_named_semaphore"); // Remove it from system
+    return 0;
+}
