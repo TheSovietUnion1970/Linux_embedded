0f5a7a49f386deb3befc22cb10a73a8bbcfcd08a
Dang Quang Vinh (MS/ETA-SEC-PF1) <dav8hc@bosch.com>
2025-10-29 15:19:04 +0700

    [KGDB][USB] - add 05_BULK_HMSC/V3/

---
+++ 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/10_USB/04_USB/05_BULK_HMSC/V3/f.h
@@ -0,0 +1,45 @@
+#ifndef F_H
+#define F_H
+
+#include "u1.h"
+#include "ms.h"
+#include <linux/math64.h>
+
+
+// Forward declaration for function parameters
+struct usb_device_data;
+
+/* BIOS Parameter Block (BPB) Details */
+typedef struct BPB {  
+    u16 Starting_LBA;
+    u16 Bytes_per_Sector;
+    u8 Sectors_per_Cluster;
+    u16 Reserved_Sectors;
+    u16 Number_of_FATs;
+    u32 Sectors_per_FAT;
+    u32 Root_Cluster;
+    u32 Data_Sector;
+} BPB;
+
+/* FAT Root Directory Entry */
+typedef struct Root_Directory_Entry {  
+    u8 File_name[11];
+    u8 File_attributes;
+    u8 Reserved;
+    u8 File_create_time;
+    u16 Create_time;
+    u16 Create_date;
+    u16 Access_date;
+    u16 High_first_cluster;
+    u16 Modified_time;
+    u16 Modified_date;
+    u16 Low_first_cluster;
+    u32 File_size; // 0 for directory
+} Root_Dir_Entry;
+
+int USB1_Read_CLUSTER(struct usb_device_data *data, u32 cluster_num, u8* cluster_data, u32* cluster_data_len, u8* name);
+
+int USB1_Read(struct usb_device_data *data);
+
+
+#endif /* F_H */
