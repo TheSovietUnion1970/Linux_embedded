adecbac0885615d13455766efb6852cff798ada4
Dang Quang Vinh (MS/ETA-SEC-PF1) <dav8hc@bosch.com>
2025-10-31 08:54:49 +0700

    Add 05_BULK_HMSC/V5/ with updated USB1_Gather_LFN_String

---
+++ 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/10_USB/04_USB/05_BULK_HMSC/V5/f.h
@@ -0,0 +1,53 @@
+#ifndef F_H
+#define F_H
+
+#include "u1.h"
+#include "ms.h"
+#include <linux/math64.h>
+
+/* Macro value */
+#define FILE_TYPE 0x20
+#define LFN_TYPE 0x0F
+
+/* Macro offser */
+#define SEQ_NUM 0x1F /* 5 bits */
+#define END_MARKER 0x40 /* bit 6 */
+
+// Forward declaration for function parameters
+struct usb_device_data;
+
+/* BIOS Parameter Block (BPB) Details */
+typedef struct BPB {  
+    u16 Starting_LBA;
+    u16 Bytes_per_Sector;
+    u8 Sectors_per_Cluster;
+    u16 Reserved_Sectors;
+    u16 Number_of_FATs;
+    u32 Sectors_per_FAT;
+    u32 Root_Cluster;
+    u32 Data_Sector;
+} BPB;
+
+/* FAT Root Directory Entry */
+typedef struct Root_Directory_Entry {  
+    u8 id;
+    u8 File_name[10];
+    u8 File_attributes;
+    u8 Reserved;
+    u8 File_create_time;
+    u16 Create_time;
+    u16 Create_date;
+    u16 Access_date;
+    u16 High_first_cluster;
+    u16 Modified_time;
+    u16 Modified_date;
+    u16 Low_first_cluster;
+    u32 File_size; // 0 for directory
+} Root_Dir_Entry;
+
+int USB1_Read_CLUSTER(struct usb_device_data *data, u32 cluster_num, u8* cluster_data, u32* cluster_data_len, u8* name, bool print_data);
+
+int USB1_Read(struct usb_device_data *data);
+
+
+#endif /* F_H */
