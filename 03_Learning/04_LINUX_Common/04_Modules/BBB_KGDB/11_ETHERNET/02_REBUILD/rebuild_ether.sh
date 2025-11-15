524bd5051240dac4e59866e51c389963eb3b229f
Vinh <vinh@vinh.com>
2025-11-15 19:21:26 +0700

    [KGDB][ETHER] - add ethernet debug

--- 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/11_ETHERNET/02_REBUILD/rebuild_ether.sh
+++ 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/11_ETHERNET/02_REBUILD/rebuild_ether.sh
@@ -4,7 +4,9 @@ export CC_x86_for_arm=/home/vinh/build_BBB_custom/gcc-11.3.0-nolibc/arm-linux-gn
 
 echo "Rebuild  drivers/net/ethernet/ti/*.ko"
 sudo make ARCH=arm CROSS_COMPILE=${CC_x86_for_arm} -C /home/vinh/build_BBB_custom/linux-stable-rcn-ee M=drivers/net/ethernet/ti modules
+sudo make ARCH=arm CROSS_COMPILE=${CC_x86_for_arm} -C /home/vinh/build_BBB_custom/linux-stable-rcn-ee M=drivers/phy/ti modules
 
 echo "Cpy *.ko to BBB"
 scp drivers/net/ethernet/ti/*.ko debian@192.168.137.2:/home/debian/ether_modules
+scp drivers/phy/ti/*.ko debian@192.168.137.2:/home/debian/ether_modules
 
--- 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/11_ETHERNET/02_REBUILD/rebuild_ether_BBB.sh
+++ 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/11_ETHERNET/02_REBUILD/rebuild_ether_BBB.sh
@@ -1,5 +1,8 @@
 echo "cp *.ko\n"
-sudo cp ether_modules/*.ko /lib/modules/5.15.177+/kernel/drivers/net/ethernet/ti/
+sudo cp ether_modules/cpsw-common.ko /lib/modules/5.15.177+/kernel/drivers/net/ethernet/ti/
+sudo cp ether_modules/davinci_mdio.ko /lib/modules/5.15.177+/kernel/drivers/net/ethernet/ti/
+sudo cp ether_modules/ti_cpsw_new.ko /lib/modules/5.15.177+/kernel/drivers/net/ethernet/ti/
+sudo cp ether_modules/phy-gmii-sel.ko /lib/modules/5.15.177+/kernel/drivers/phy/ti/
 
 echo "update initramfs for ether\n"
 sudo update-initramfs -u -k $(uname -r)
