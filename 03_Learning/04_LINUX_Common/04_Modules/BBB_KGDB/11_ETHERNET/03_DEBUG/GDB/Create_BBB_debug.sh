3f7681843ad7523dad4a48060e4ad180f03830ba
Vinh <vinh@vinh.com>
2025-11-16 08:38:32 +0700

    [KGDB][ETHER] - add 11_ETHERNET/03_DEBUG/

---
+++ 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/11_ETHERNET/03_DEBUG/GDB/Create_BBB_debug.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Usage: ./make_gdb_script.sh X
+# Example: ./make_gdb_script.sh 4
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 X"
+    exit 1
+fi
+
+X=$1
+
+# Read symbol addresses directly from /sys/module
+a1=$(sudo cat /sys/module/ti_cpsw_new/sections/.text)
+a2=$(sudo cat /sys/module/davinci_mdio/sections/.text)
+a3=$(sudo cat /sys/module/cpsw_common/sections/.text)
+b1=$(sudo cat /sys/module/phy_gmii_sel/sections/.text)
+
+cat > BBB_debug.gdb <<EOF
+set serial baud 115200
+target remote /dev/pts/${X}
+
+add-symbol-file /home/vinh/build_BBB_custom/linux-stable-rcn-ee/drivers/net/ethernet/ti/ti_cpsw_new.ko ${a1}
+add-symbol-file /home/vinh/build_BBB_custom/linux-stable-rcn-ee/drivers/net/ethernet/ti/davinci_mdio.ko ${a2}
+add-symbol-file /home/vinh/build_BBB_custom/linux-stable-rcn-ee/drivers/net/ethernet/ti/cpsw_common.ko ${a3}
+
+add-symbol-file /home/vinh/build_BBB_custom/linux-stable-rcn-ee/drivers/phy/ti/phy_gmii_sel.ko ${b1}
+
+
+EOF
+
+echo "BBB_debug.gdb created with live module addresses."
+
+echo "Cpy back to PC."
+sudo scp BBB_debug.gdb vinh@192.168.137.1:/home/vinh/
+
