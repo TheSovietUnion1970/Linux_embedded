1d9fdc7cf5e70ab449a9753db6f3e9623f7315c6
Dang Quang Vinh (MS/ETA-SEC-PF1) <dav8hc@bosch.com>
2025-12-17 11:17:57 +0700

    [KGDB][ETHER] - update parse.c

--- 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/11_ETHERNET/05_ETHER_FRAME/Parsing_header_C/parse.c
+++ 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/11_ETHERNET/05_ETHER_FRAME/Parsing_header_C/parse.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "parse.h"
 
+void print_ether_header(const struct ether_header *eth) {
+    printf("=== Ether Header ===\n");
+    printf("Destination MAC    : %x:%x:%x:%x:%x:%x\n", eth->MAC_dst[0], eth->MAC_dst[1], eth->MAC_dst[2], eth->MAC_dst[3], eth->MAC_dst[4], eth->MAC_dst[5]);
+    printf("Source MAC         : %x:%x:%x:%x:%x:%x\n", eth->MAC_src[0], eth->MAC_src[1], eth->MAC_src[2], eth->MAC_src[3], eth->MAC_src[4], eth->MAC_src[5]);
+    printf("Type               : %u\n", eth->type);
+}
 
 void print_ipv4_header(const struct ipv4_header *ip) {
     printf("=== IPv4 Header ===\n");
@@ -25,11 +31,15 @@ void print_ipv4_header(const struct ipv4_header *ip) {
     }
     printf("Header Checksum    : 0x%04x\n", ntohs(ip->header_checksum));
     printf("Source IP          : %u.%u.%u.%u\n",
-           (ip->source_ip >> 24) & 0xFF, (ip->source_ip >> 16) & 0xFF,
-           (ip->source_ip >> 8) & 0xFF,  ip->source_ip & 0xFF);
+           ip->source_ip & 0xFF, 
+           (ip->source_ip >> 8) & 0xFF,
+           (ip->source_ip >> 16) & 0xFF,  
+           (ip->source_ip >> 24) & 0xFF);
     printf("Destination IP     : %u.%u.%u.%u\n",
-           (ip->dest_ip >> 24) & 0xFF, (ip->dest_ip >> 16) & 0xFF,
-           (ip->dest_ip >> 8) & 0xFF,  ip->dest_ip & 0xFF);
+           ip->dest_ip & 0xFF, 
+           (ip->dest_ip >> 8) & 0xFF,
+           (ip->dest_ip >> 16) & 0xFF,  
+           (ip->dest_ip >> 24) & 0xFF);
 }
 
 void print_icmp_header(const struct icmp_header *icmp) {
@@ -72,13 +82,17 @@ void print_tcp_header(const struct tcp_header *tcp) {
 }
 
 // Example usage: parse a raw packet buffer (starting after Ethernet header)
-void parse_and_print_packet(const uint8_t *packet, size_t len) {
+void parse_and_print_packet(const uint8_t *packet, size_t len, uint8_t* name_packet) {
     if (len < sizeof(struct ipv4_header)) {
         printf("Packet too short for IPv4 header\n");
         return;
     }
 
-    const struct ipv4_header *ip = (const struct ipv4_header *)packet;
+    const struct ether_header *eth = (const struct ether_header *)((uint8_t*)packet);
+    const struct ipv4_header *ip = (const struct ipv4_header *)((uint8_t*)packet + sizeof(struct ether_header));
+
+    printf("================== %s ==================\n", name_packet);
+    print_ether_header(eth);
     print_ipv4_header(ip);
 
     size_t ip_hdr_len = ip->ihl * 4;
@@ -89,7 +103,7 @@ void parse_and_print_packet(const uint8_t *packet, size_t len) {
 
     if (ip->protocol == 1) {  // ICMP
         if (len >= ip_hdr_len + sizeof(struct icmp_header)) {
-            const struct icmp_header *icmp = (const struct icmp_header *)(packet + ip_hdr_len);
+            const struct icmp_header *icmp = (const struct icmp_header *)(packet + ip_hdr_len + 14);
             print_icmp_header(icmp);
         }
     } else if (ip->protocol == 6) {  // TCP
@@ -98,5 +112,7 @@ void parse_and_print_packet(const uint8_t *packet, size_t len) {
             print_tcp_header(tcp);
         }
     }
+
+    printf("================== === === ==================\n");
 }
 
