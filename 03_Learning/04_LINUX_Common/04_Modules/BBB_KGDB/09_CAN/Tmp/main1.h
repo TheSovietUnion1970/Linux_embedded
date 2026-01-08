aa867706d2607ec23ae07f7fe053404b7d01f4dd
Vinh <vinh@vinh.com>
2026-01-08 08:00:51 +0700

    [KGDB][CAN] - add tmp for shared tx and rx

---
+++ 03_Learning/04_LINUX_Common/04_Modules/BBB_KGDB/09_CAN/Tmp/main1.h
@@ -0,0 +1,16 @@
+#ifndef MAIN1_H
+#define MAIN1_H
+
+
+/* CONTROL */
+#define DMA_OBJ_MSG_NUM_TX 12
+#define DMA_OBJ_MSG_NUM_RX 13
+#define DMA_USED 0
+
+
+#define DMA_REG 1
+#define ID_sent 0x40
+#define ID_received 0x50
+/* ******* */
+
+#endif
