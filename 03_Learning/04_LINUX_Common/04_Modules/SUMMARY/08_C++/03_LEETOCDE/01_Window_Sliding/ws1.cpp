9f9c5b5127b7188fba66796d31bebbb6404ffcac
Vinh <vdang9646@gmail.com>
2026-07-19 09:52:29 +0700

    [C++] - Add ws1.cpp

---
+++ 03_Learning/04_LINUX_Common/04_Modules/SUMMARY/08_C++/03_LEETOCDE/01_Window_Sliding/ws1.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <climits>
+using namespace std;
+ 
+class WindowSliding {
+private:
+    std::string name;
+public:
+    /*
+        l = left
+        r = right
+    */
+    WindowSliding (std::string n) : name(n){}
+
+    /*
+        0. Maximum len of equal sub arr when removing kth elements
+            + {1,2,3,1,2,3,1}, k = 4
+            -> {1,1,1}, max len = 3 -> remove 2,3,2,3
+            + {1,2,3,1,2,3,1}, k = 3
+            -> {1,1}, max len = 2 -> remove first 2,3 or second 2,3
+
+        1. {1,2,3,1,2,3,1}
+        -> we gather all same number into ordered index:
+            + {1,1,1}
+            + {2,2}
+            + {3,3}
+            -> max len = 3, {1,1,1}
+                                    
+        2. The substract of real idx and same array idx
+            + {1,2,3,1,2,3,1}
+            0     3
+            + {1,1,1}
+            0 1
+
+            => 3 - 0 - (1 - 0) = 2 -> 2 here is the len {2,3} between 1 value
+
+    */
+    int MaxLen(std::vector<int>& vec, int k_removed){
+        int l = 0, r = 0;
+        std::unordered_map<int, std::vector<int>> Val2Vector;
+        int max_len = 0;
+
+        for (int i = 0; i < vec.size(); i++){
+            Val2Vector[vec.at(i)].push_back(i);
+        }
+
+        for (auto pair : Val2Vector){
+            std::vector<int> vec = pair.second;
+
+            for (r = 0; r < vec.size(); r++){
+                while (vec[r] - vec[l] - (r - l) > k_removed){
+                    l++;
+                }
+
+                max_len = std::max(r - l + 1, max_len);
+            }
+        }
+
+        return max_len;
+    }
+ 
+    /*
+    0. With at most m subarray, len = [l,r] -> Find the max sum of at most m subarray
+        + {-1, 7,-4}, m=1, l=2, r=3 
+            -> max sum = 6, 
+            -> {-1,7}
+
+        + {1, 2, 3, 4, 5}, m=2, l=1, r=3
+            -> max sum = 15, 
+            -> ({1, 2}, {3, 4, 5}) or ({1, 2, 3}, {4, 5})
+
+    CASE 1:
+        DP Table: Maximum Sum using first i elements with exactly j subarrays
+        (l=1, r=3, m=2)
+
+        nums   = [1, 2, 3, 4, 5]
+        prefix = [0, 1, 3, 6, 10, 15]
+
+                            <-------- m ------->
+                            j=0     j=1      j=2
+        |  i=0  []             0      -        -
+        |  i=1  [1]            0      1        -
+        n  i=2  [1,2]          0      3        3
+        +  i=3  [1,2,3]        0      6        6
+        1  i=4  [1,2,3,4]      0      10       10
+        |  i=5  [1,2,3,4,5]    0      15       15   ← Answer
+
+
+    CASE 2:
+        nums = [-1, 7,-4], m=1, l=2, r=3
+        prefix [ 0,-1, 6, 2]
+        +---------------+-------+-------+
+        | i \ j         |  j=0  |  j=1  |
+        +---------------+-------+-------+
+        | 0  []         |   0   |   -   |
+        | 1  [-1]       |   0   |  -1   |
+        | 2  [-1,7]     |   0   |   6   |
+        | 3  [-1,7,-4]  |   0   |   2   |
+        +---------------+-------+-------+
+
+    */
+    int MaximumSum(vector<int>& nums, int m, int l, int r){
+        int n = nums.size();
+        std::vector<int> prefix(n+1, 0);
+        //std::vector<int, std::vector<int>> Vec2MaxSumPerSubarray;
+        int Vec2MaxSumPerSubarray[n+1][m+1] = {INT_MIN};
+        
+        prefix[0] = 0;
+        for (int i = 1; i <= n; i++){
+            prefix[i] = prefix[i-1] + nums.at(i-1);
+        }
+
+        Vec2MaxSumPerSubarray[0][0] = 0;
+        for (int i = 1; i <= n; i++){
+            Vec2MaxSumPerSubarray[i][0] = 0;
+            for (int j = 1; j <= std::min(i,m); j++){
+                
+                for (int len = l; len <= r; len++){
+                    if (i-len >= 0){
+                        int subsum = prefix[i] - prefix[i-len];
+                        int sum = subsum + Vec2MaxSumPerSubarray[i-len][j-1];
+                        // std::cout << subsum << std::endl;
+                        Vec2MaxSumPerSubarray[i][j] = std::max(sum, Vec2MaxSumPerSubarray[i][j]);
+
+                        // also compare with the previous j-1
+                        Vec2MaxSumPerSubarray[i][j] = std::max(Vec2MaxSumPerSubarray[i][j], Vec2MaxSumPerSubarray[i-1][j]);
+                    }
+                    //std::cout << Vec2MaxSumPerSubarray[i][j] << std::endl;
+                }
+
+            }
+        }
+
+        int res = INT_MIN;
+        for (int j = 1; j <= m; j++){
+            // std::cout << Vec2MaxSumPerSubarray[n][j] << std::endl;
+            res = std::max(Vec2MaxSumPerSubarray[n][j], res);
+        }
+
+        return res;
+    }
+
+};
+ 
+int main(){
+    WindowSliding ws("Window Sliding");
+
+    std::vector<int> v = {1,2,3,1,2,3,1};
+    int max = ws.MaxLen(v,3);
+    std::cout << "MaxLen = " << max << std::endl;
+
+    std::vector<int> v1 = {-1,7,-4};
+    max = ws.MaximumSum(v1, 1,2,3);
+    std::cout << "MaximumSum = " << max << std::endl;
+    return 0;
+}
